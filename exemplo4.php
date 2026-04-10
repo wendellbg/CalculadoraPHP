7f759bfa39e99d7c23b3e9944de0126ac87e764b
Wendell Bento Geraldes <wendellbg@gmail.com>
2026-04-10 10:22:42 -0300

    Arquivo de teste
    Este arquivo é uma tabuada que estou usando para testar o meu repositório no githhub

---
+++ exemplo4.php
@@ -0,0 +1,15 @@
+<?php
+echo "<h1>TABUADA DE 7</h1><br>";
+$tabuada = 7;
+echo "$tabuada x 1 =".$tabuada*1,"<br>";
+echo "$tabuada x 2 =".$tabuada*2,"<br>";
+echo "$tabuada x 3 =".$tabuada*3,"<br>";
+echo "$tabuada x 4 =".$tabuada*4,"<br>";
+echo "$tabuada x 5 =".$tabuada*5,"<br>";
+echo "$tabuada x 6 =".$tabuada*6,"<br>";
+echo "$tabuada x 7 =".$tabuada*7,"<br>";
+echo "$tabuada x 8 =".$tabuada*8,"<br>";
+echo "$tabuada x 9 =".$tabuada*9,"<br>";
+echo "$tabuada x 10 =".$tabuada*10,"<br>";
+?>
+
